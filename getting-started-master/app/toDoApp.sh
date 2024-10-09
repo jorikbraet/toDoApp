4093f930c79181b8b32ca1a0e93c91914750a38f
Jorik Braet <jorik.braet@student.hogent.be>
2024-10-09 23:17:15 +0200

    debug

--- getting-started-master/app/toDoApp.sh
+++ getting-started-master/app/toDoApp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")" 
+if [ ! -f "$PWD/Dockerfile" ]; then
+    echo "Error: Dockerfile not found in the current directory!"
+    exit 1
+fi
 docker build -t getting-started .
 docker run -dp 3000:3000 getting-started
 docker ps -a 
